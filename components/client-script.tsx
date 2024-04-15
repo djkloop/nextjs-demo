1a255e03e5a8368f792362310405e1aeff3a1a59
zhangjianlei <jingyingbangkeji/zhangjianlei>
2024-04-15 17:36:27 +0800

    update

---
+++ components/client-script.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Script from "next/script"
+
+const ClientScript = () => {
+    return <>
+               <Script src="https://cdn.wwads.cn/js/makemoney.js" async type="text/javascript" />
+    </>
+}
+
+export default ClientScript
